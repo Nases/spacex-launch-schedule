50469a2e2138aa38bc1dc478aff89fdd963518da
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-12 22:59:42 -0700

    🚀

--- components/Launches.js
+++ components/Launches.js
@@ -17,12 +17,11 @@ export default () => {
 
   function getYoutubeEmbedLink(url) {
     if (!url) return undefined
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-    const match = url.match(regExp);
-
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+    const match = url.match(regExp)
     return (match && match[2].length === 11)
       ? 'https://www.youtube.com/embed/' + match[2]
-      : null;
+      : null
   }
 
   return (
