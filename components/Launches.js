79550d97ea1dfe8acf99b694e316cb78db9f2c2b
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-16 11:54:21 -0700

    🚀

--- components/Launches.js
+++ components/Launches.js
@@ -55,7 +55,7 @@ export default () => {
                 <th className="sm:px-6 sm:py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                   Mission
                 </th>
-                <th onClick={() => reverseLaunches()} className="hidden sm:table-cell text-center px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider cursor-pointer">
+                <th onClick={() => reverseLaunches()} className="text-center sm:px-6 sm:py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider cursor-pointer">
                   Launch Date &nbsp;
                   {(isLaunchesReversed) ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
                 </th>
