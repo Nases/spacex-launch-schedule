41cd518cef5583eab0e2ec0b6e2ceb46f06b26b3
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-16 12:26:32 -0700

    🚀

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import ReactGA from 'react-ga'
 
-const trackingId = "UA-160643323-1"
-ReactGA.initialize(trackingId)
 
 export default ({ children }) => {
+  const trackingId = "UA-160643323-1"
+  ReactGA.initialize(trackingId)
+
   return (
     <div>
       <Head>
