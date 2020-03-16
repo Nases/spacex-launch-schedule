e144e8dd86ddd8e48c4c130c35d05209c5c0b9b8
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-15 17:05:32 -0700

    🚀

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" /> */}
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" /> */}
+          <meta content="width=640, initial-scale=0.47, maximum-scale=1.0, user-scalable=1" name="viewport" />
+
           <meta name="description" content="Detailed information about SpaceX launches 🚀" />
           <link rel="shortcut icon" href="/favicon.png" />
           <script src="https://kit.fontawesome.com/c9c5a1adc4.js" crossorigin="anonymous"></script>
