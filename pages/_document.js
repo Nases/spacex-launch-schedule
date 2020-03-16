2befa551ae78480410950249afeeaaf8162c23b5
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-16 13:09:46 -0700

    🚀

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="Detailed information about SpaceX launches 🚀" />
+          <meta name="description" content="Get detailed information about SpaceX launches 🚀" />
           <link rel="shortcut icon" href="/favicon.png" />
           <script src="https://kit.fontawesome.com/c9c5a1adc4.js" crossorigin="anonymous"></script>
         </Head>
