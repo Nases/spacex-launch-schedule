28a6e95e0912d0a5c20dbe2205cfff323457ef67
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-08 21:35:05 -0800

    '🚀'

--- components/Launches.js
+++ components/Launches.js
@@ -57,7 +57,7 @@ const Launches = ({ launchesData }) => {
         type="button"
         className={currentPage == pageNumber
           ?
-          "-ml-px relative inline-flex items-center px-4 py-2 cursor-auto border border-gray-300 bg-blue-600 text-sm leading-5 font-medium text-white transition ease-in-out duration-150"
+          "-ml-px relative inline-flex items-center px-4 py-2 cursor-auto border border-gray-300 bg-gray-700 text-sm leading-5 font-medium text-white transition ease-in-out duration-150"
           :
           "-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white hover:bg-gray-200 text-sm leading-5 font-medium text-gray-700 transition ease-in-out duration-150"
         }
