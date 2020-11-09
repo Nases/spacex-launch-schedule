31a67abf011e34c034492395661357f801c80d5a
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-08 21:31:07 -0800

    '🚀'

--- pages/launch/[flight_number].js
+++ pages/launch/[flight_number].js
@@ -182,6 +182,17 @@ const LaunchDetails = () => {
                         </iframe>
                       </Modal>
                     </dl>
+                    {(rocket_name === 'Falcon 9') ?
+                      <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
+                        <dt className="text-sm leading-5 font-medium text-gray-500">
+                          Mission cost
+                        </dt>
+                        <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
+                          ~$50.000.000
+                        </dd>
+                      </div>
+                      : ''
+                    }
                     {patch
                       ?
                       <div className="mt-8 sm:mt-0 sm:grid sm:grid-cols-3 sm:gap-4 sm:border-t sm:border-gray-200 sm:px-6 sm:py-5">
