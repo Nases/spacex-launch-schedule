f2869b6000b1f115248dff1672e69eff3e7d06cd
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-16 02:24:47 -0800

    '🚀'

--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -22,7 +22,7 @@ const TermsAndConditions = () => {
               </BreadCrumbs>
               <br />
               <h1><strong>Terms and Conditions</strong></h1>
-              <p>Welcome to SpaceX Launch Schedule!</p>
+              <p>Welcome to SpaceX Launch Schedule.</p>
               <p>These terms and conditions outline the rules and regulations for the use of spacexschedule.com/'s Website, located at spacexschedule.com/.</p>
               <p>By accessing this website we assume you accept these terms and conditions. Do not continue to use SpaceX Launch Schedule if you do not agree to take all of the terms and conditions stated on this page.</p>
               <p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
