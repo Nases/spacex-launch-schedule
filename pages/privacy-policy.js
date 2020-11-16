2bc08a85ca43507c3a0552062a505426b8aaac24
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-11-16 02:24:20 -0800

    '🚀'

--- pages/privacy-policy.js
+++ pages/privacy-policy.js
@@ -22,7 +22,7 @@ const PrivacyPolicy = () => {
               </BreadCrumbs>
               <br />
               <h1><strong>Privacy Policy</strong></h1>
-              <p>At SpaceX Launch Schedule, accessible from spacex-launches.hasansefaozalp.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by SpaceX Launch Schedule and how we use it.</p>
+              <p>At SpaceX Launch Schedule, accessible from spacexschedule.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by SpaceX Launch Schedule and how we use it.</p>
               <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
               <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in SpaceX Launch Schedule. This policy is not applicable to any information collected offline or via channels other than this website.</p>
               <br />
